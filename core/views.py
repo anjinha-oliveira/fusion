fbf1dd93a6bcf2a6f58e603867b78f415fe49839
Angela Oliveira <oliveirqangela96@gmail.com>
2024-02-06 17:29:17 -0300

    Adicionando Class Based Views

--- core/views.py
+++ core/views.py
@@ -1,8 +1,19 @@
+from typing import Any
 from django.shortcuts import render
 
 from django.views.generic import TemplateView
+from .models import Servico, Funcionario, Funcionalidades
 
 #Exemplo de Class Based Views
 
 class indexView(TemplateView):
     template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(indexView, self).get_context_data(**kwargs)
+        context['servicos'] = Servico.objects.order_by('?').all()
+        context['funcionarios'] = Funcionario.objects.order_by('?').all()
+        return context
+
+
+
