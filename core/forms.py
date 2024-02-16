a384b50d82dd478d3a95dc54496c335c9787679b
Angela Oliveira <oliveirqangela96@gmail.com>
2024-02-16 11:43:28 -0300

    implementando send_mail, para extrair informações do formulário

--- core/forms.py
+++ core/forms.py
@@ -3,6 +3,12 @@ from django.core.mail.message import EmailMessage
 
 class ContatoForms(forms.Form):
     nome = forms.CharField(label='Nome', max_length=100)
-    email = forms.EmailField(label='Email', max_length=100)
+    email = forms.EmailField(label='E-mail', max_length=100)
     assunto = forms.CharField(label='Assunto', max_length=100)
     mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
+
+    def send_mail(self):
+        nome = self.cleaned_data['nome']
+        email = self.cleaned_data['email']
+        assunto = self.cleaned_data['assunto']
+        mensagem = self.cleaned_data['mensagem']
