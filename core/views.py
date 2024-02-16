6b5392f848a869f39a0eca8d5126ebef5cee9610
Angela Oliveira <oliveirqangela96@gmail.com>
2024-02-16 12:05:58 -0300

    Importando o FormView e incluindo na classe indexView

--- core/views.py
+++ core/views.py
@@ -1,12 +1,9 @@
-from typing import Any
-from django.shortcuts import render
-
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 from .models import Servico, Funcionario, Funcionalidades
 
 #Exemplo de Class Based Views
 
-class indexView(TemplateView):
+class indexView(TemplateView, FormView):
     template_name = 'index.html'
     
     def get_context_data(self, **kwargs):
