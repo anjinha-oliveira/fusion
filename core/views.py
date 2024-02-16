07342af16bb47c0a8baf5f165eeb4154cb087782
Angela Oliveira <oliveirqangela96@gmail.com>
2024-02-16 15:06:04 -0300

    Adicionando form_invalid para retornar formulário em caso de erro e exibir mensagem de erro ao usuário

--- core/views.py
+++ core/views.py
@@ -25,5 +25,9 @@ class indexView(TemplateView, FormView):
         form.send_mail()
         messages.success(self.request, 'E-mail enviado com sucesso')
         return super(indexView, self).form_valid(form, *args, **kwargs)
+    
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Erro ao enviar e-mail')
+        return super(indexView, self).form_invalid(form, *args, **kwargs)
 
 
