0c79b4bc42a8bd3d62a6a379e37a4c997cbe8970
Angela Oliveira <oliveirqangela96@gmail.com>
2024-02-16 14:50:20 -0300

    fazendo o processamento do formulário na view

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.core.mail.message import EmailMessage
 
-class ContatoForms(forms.Form):
+class ContatoForm(forms.Form):
     nome = forms.CharField(label='Nome', max_length=100)
     email = forms.EmailField(label='E-mail', max_length=100)
     assunto = forms.CharField(label='Assunto', max_length=100)
--- core/views.py
+++ core/views.py
@@ -1,10 +1,15 @@
 from django.views.generic import TemplateView, FormView
+from django.urls import reverse_lazy
+
 from .models import Servico, Funcionario, Funcionalidades
+from .forms import ContatoForm
 
 #Exemplo de Class Based Views
 
 class indexView(TemplateView, FormView):
     template_name = 'index.html'
+    form_class = ContatoForm
+    success_url = reverse_lazy('index')
     
     def get_context_data(self, **kwargs):
         context = super(indexView, self).get_context_data(**kwargs)
