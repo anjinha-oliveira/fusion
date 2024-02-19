9c08d8646f861cc8df1d5b8f5980c5973c7b854d
Angela Oliveira <oliveirqangela96@gmail.com>
2024-02-19 17:24:55 -0300

    Atualizações para deploy no vercel

--- fusion/settings.py
+++ fusion/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = "django-insecure-uxm2c-+mx=3rc($-!d@-+6pxa1^&-1(ch6irlih*8wv^21z#9^
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
