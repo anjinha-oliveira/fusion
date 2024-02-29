f1e7c6e6dd23a60a66f221d2a423cbe3b74f1dab
Angela Oliveira <oliveirqangela96@gmail.com>
2024-02-29 10:21:38 -0300

    Atualizando dados da seção de feedbacks no dump de feedbacks

--- build_files.sh
+++ build_files.sh
@@ -5,6 +5,6 @@ python3.9 manage.py loaddata core/fixtures/funcionalidades.json
 python3.9 manage.py loaddata core/fixtures/funcionario.json 
 python3.9 manage.py loaddata core/fixtures/servico.json 
 python3.9 manage.py loaddata core/fixtures/user.json
-python3.9 manage.py loaddata core/fixtures/clientes.json 
+python3.9 manage.py loaddata core/fixtures/feedbacks.json 
 
 python3.9 manage.py collectstatic
