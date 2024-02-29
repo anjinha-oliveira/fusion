4729849a6c851260622c6a7fafe85d063e24ba2a
Angela Oliveira <oliveirqangela96@gmail.com>
2024-02-29 10:11:35 -0300

    Adicionando fixtures de clientes

--- build_files.sh
+++ build_files.sh
@@ -4,5 +4,7 @@ python3.9 manage.py loaddata core/fixtures/cargo.json
 python3.9 manage.py loaddata core/fixtures/funcionalidades.json
 python3.9 manage.py loaddata core/fixtures/funcionario.json 
 python3.9 manage.py loaddata core/fixtures/servico.json 
-python3.9 manage.py loaddata core/fixtures/user.json 
+python3.9 manage.py loaddata core/fixtures/user.json
+python3.9 manage.py loaddata core/fixtures/clientes.json 
+
 python3.9 manage.py collectstatic
