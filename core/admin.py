975bae88fa25ea654315a80ce03d343db2e54b8c
Angela Oliveira <oliveirqangela96@gmail.com>
2024-03-11 19:38:16 -0300

    Adicionando novo formato de preenchimento de estrelas ao admin e fazendo a implementação no template

--- core/admin.py
+++ core/admin.py
@@ -5,31 +5,29 @@ from .models import Cargo, Servico, Funcionario, Funcionalidades, Feedbacks
 
 @admin.register(Cargo)
 class CargoAdmin(admin.ModelAdmin):
-    list_display = ('cargo', 'ativo', 'modificado')
+    list_display = ("cargo", "ativo", "modificado")
 
 
 @admin.register(Servico)
 class ServicoAdmin(admin.ModelAdmin):
-    list_display = ('servico', 'icone', 'ativo', 'modificado')
+    list_display = ("servico", "icone", "ativo", "modificado")
 
 
 @admin.register(Funcionario)
 class FuncionarioAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'cargo', 'ativo', 'modificado')
+    list_display = ("nome", "cargo", "ativo", "modificado")
+
 
 @admin.register(Funcionalidades)
 class FuncionalidadesAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'descricao', 'icone')
+    list_display = ("nome", "descricao", "icone")
+
 
 @admin.register(Feedbacks)
 class FeedbacksAdmin(admin.ModelAdmin):
     list_display = (
-        'nome', 
-        'profissao', 
-        'feedbacks', 
-        'estrela_1',
-        'estrela_2',
-        'estrela_3',
-        'estrela_4',
-        'estrela_5', 
+        "nome",
+        "profissao",
+        "feedbacks",
+        "estrelas",
     )
